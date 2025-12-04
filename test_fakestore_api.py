eba0724c2ca78f3d54d7976f42b958346b661bdd
celina2123 <celinaelheddad@gmail.com>
2025-12-04 10:48:16 +0100

    Delete test_fakestore_api.py

--- test_fakestore_api.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-BASE_URL = "https://fakestoreapi.com"
-
-def test_products_returns_200():
-    response = requests.get(f"{BASE_URL}/products", timeout=10)
-    assert response.status_code == 200
